270365dac0b1c766a4cc34aa978f4f44a6c278c4
Gatis Brediks <gatis.brediks@gmail.com>
2022-02-21 22:26:34 +0200

    updated project

--- OOP_training/Rumbula.php
+++ OOP_training/Rumbula.php
@@ -4,16 +4,15 @@ echo str_repeat('-', 10) . "RUMBULA" . str_repeat('-', 10) . PHP_EOL;
 
 class Car
 {
-    public int $index;
-    public string $make;
-    public string $model;
-    public int $firstRegistrationYear;
-    public string $color;
-    public int $price;
-    public bool $isAvailable;
+    private int $index;
+    private string $make;
+    private string $model;
+    private int $firstRegistrationYear;
+    private string $color;
+    private int $price;
 
     public function __construct(int $index, string $make, string $model, int $firstRegistrationYear,
-                                string $color, int $price, bool $isAvailable)
+                                string $color, int $price)
     {
         $this->index = $index;
         $this->make = $make;
@@ -21,38 +20,39 @@ class Car
         $this->firstRegistrationYear = $firstRegistrationYear;
         $this->color = $color;
         $this->price = $price;
-        $this->isAvailable = $isAvailable;
     }
 
-//    public function getAvailableCarsList()
-//    {
-//        if (!$this->isAvailable = true) {
-//            die ("This car is not available");
-//        } else {
-//            $result = $this->index . ', Model: '
-//                . $this->make . ' '
-//                . $this->model . ', year: '
-//                . $this->firstRegistrationYear . ', color: '
-//                . $this->color . ', price (EUR): '
-//                . $this->price;
-//        }
-//        return $result;
-//    }
-//}
+    public function getIndex(): int
+    {
+        return $this->index;
+    }
+
+    public function getMake(): string
+    {
+        return $this->make;
+    }
+
+    public function getModel(): string
+    {
+        return $this->model;
+    }
+
+    public function getFirstRegistrationYear(): int
+    {
+        return $this->firstRegistrationYear;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
 }
 
-$car1 = new Car(00, 'BMW', '530 Gran Turismo', 2010, 'dark grey', 13500, true);
-$car2 = new Car(01, 'Jaguar', 'XF', 2008, 'white', 6500, true);
-$car3 = new Car(02, 'Nissan', 'Qashqai', 2016, 'black', 12500, true);
-$car4 = new Car(03, 'SAAB', '9-5 Aero', 2004, 'dark blue', 2000, true);
-$car5 = new Car(04, 'Volvo', 'V70', 2006, 'silver', 3500, true);
-
-
-//echo $car1->getAvailableCarsList() . PHP_EOL;
-//echo $car2->getAvailableCarsList() . PHP_EOL;
-//echo $car3->getAvailableCarsList() . PHP_EOL;
-//echo $car4->getAvailableCarsList() . PHP_EOL;
-//echo $car5->getAvailableCarsList() . PHP_EOL;
 
 class Shop
 {
@@ -63,6 +63,13 @@ class Shop
 //        return $this->availableCars[] = $car;
 //    }
 }
+
+
+$car1 = new Car(00, 'BMW', '530 Gran Turismo', 2010, 'dark grey', 13500);
+$car2 = new Car(01, 'Jaguar', 'XF', 2008, 'white', 6500);
+$car3 = new Car(02, 'Nissan', 'Qashqai', 2016, 'black', 12500);
+$car4 = new Car(03, 'SAAB', '9-5 Aero', 2004, 'dark blue', 2000);
+$car5 = new Car(04, 'Volvo', 'V70', 2006, 'silver', 3500);
 
 $availableCars = new Shop();
 
